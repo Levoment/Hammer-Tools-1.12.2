034cef7975f9dd8561a0ab81b5306d0079e40092
BuildTools <unconfigured@null.spigotmc.org>
2018-12-14 04:34:23 -0600

    Crafting Recipes

--- src/main/java/com/levoment/hammertools/util/RegistryHandler.java
+++ src/main/java/com/levoment/hammertools/util/RegistryHandler.java
@@ -3,7 +3,9 @@ package com.levoment.hammertools.util;
 import com.levoment.hammertools.init.ModItems;
 import com.levoment.hammertools.items.ItemHammer;
 import com.levoment.hammertools.items.ItemSuperAxe;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,9 +16,24 @@ public class RegistryHandler {
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> registryEvent) {
+        // The creative tabs variable for this mod
+        CreativeTabs tabHammerTools = new CreativeTabs("tab_levoment_hammerTools") {
+            // Return the icon for the creative tab
+
+            @Override
+            public ItemStack getTabIconItem() {
+                return new ItemStack(ModItems.itemDiamondHammer);
+            }
+        };
+        
         for (Item item : ModItems.ITEMS) {
             registryEvent.getRegistry().register(item);
+            //Set the creative tab for the item
+            item.setCreativeTab(tabHammerTools);
         }
+
+
+
     }
 
     @SubscribeEvent
